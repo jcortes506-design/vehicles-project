9f3cf9b32db41700ac378d57defe51724b1fd5fc
jcortes506-design <jcortes506@gmail.com>
2025-09-26 21:10:16 -0600

    update README.md y add app.py con gráficos

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vehicles-project
 Exploratory Data Analysis de un dataset de vehículos con pandas, streamlit y plotly
+Esta aplicacion web permite:
+- Construir un histograma de la columan odometer 
+- Construir un grafico de dispersion enter odometer y price
+
---
+++ app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('./vehicles_us.csv')
+
+st.header('Exploratory Data Analysis - Vehicles Dataset')
+
+hist_buttom = st.button('Construir Histograma')
+
+if hist_buttom:
+    st.write('Creacion de un histograma para odometer')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_buttom = st.button('Construir diagrama de dispersion')
+
+if scatter_buttom:
+    st.write('Creacion de un grafico de dispersion entre odometer y price')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
+
+build_hist = st.checkbox("Mostrar histograma (odometer)")
+build_scatter = st.checkbox("Mostrar dispersion (odometer vs price)")
+
+if build_hist:
+    st.write("Histograma de odometer")
+    fig = px.histogram(data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if build_scatter:
+    st.write("Dispersion odometer vs price")
+    fig = px.scatter(data, x="odometer", y="price",opacity=0.6)
+    st.plotly_chart(fig, use_container_width=True)
